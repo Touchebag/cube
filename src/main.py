5838e2864b57f1964f54d13d1651a22efae9ccda
Touchebag <someone90_@hotmail.com>
2025-02-20 19:25:57 +0100

    Removed address.txt dependency

--- src/main.py
+++ src/main.py
@@ -7,18 +7,15 @@ from window import MainWindow
 import global_state
 
 async def main_loop():
-    with open('address.txt') as f:
-        mac_address = f.read().replace("\n", "")
+    global_state.main_window = MainWindow()
 
-        global_state.main_window = MainWindow()
+    while not global_state.main_window.close:
+        global_state.main_window.render()
 
-        while not global_state.main_window.close:
-            global_state.main_window.render()
+        if global_state.cube_comm.connection_status.get() == "Not connected":
+            asyncio.create_task(global_state.cube_comm.scan_and_connect())
 
-            if global_state.cube_comm.connection_status.get() == "Not connected":
-                asyncio.create_task(global_state.cube_comm.scan_and_connect())
-
-            await asyncio.sleep(0.1)
+        await asyncio.sleep(0.1)
 
 
 if __name__ == "__main__":
